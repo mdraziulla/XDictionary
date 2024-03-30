bc90a1adbd77fd29c3c037a8700057f63437ad8e
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-30 22:07:09 +0530

    XDictionary

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import DictionaryApp from './XDictionary'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <DictionaryApp/>
+      </div>
+    </>
+  )
+}
+
+export default App
